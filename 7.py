f2b3931129bc8b5e59c9bb39a677f43fbea0561c
Debarko De <debarko89@gmail.com>
2018-01-19 19:58:41 +0530

    First commit

---
+++ 7.py
@@ -0,0 +1,257 @@
+from numpy import argmax
+from pickle import load
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from nltk.translate.bleu_score import corpus_bleu
+import urllib
+import sys
+
+from os import listdir
+from pickle import dump
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+
+# extract features from each photo in the directory
+
+
+def extract_features(directory):
+    # load the model
+    model = VGG16()
+    # re-structure the model
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+    # extract features from each photo
+    features = dict()
+    for name in listdir(directory):
+        # load an image from file
+        filename = directory + '/' + name
+        image = load_img(filename, target_size=(224, 224))
+        # convert the image pixels to a numpy array
+        image = img_to_array(image)
+        # reshape data for the model
+        image = image.reshape(
+            (1, image.shape[0], image.shape[1], image.shape[2]))
+        # prepare the image for the VGG model
+        image = preprocess_input(image)
+        # get features
+        feature = model.predict(image, verbose=0)
+        # get image id
+        image_id = name.split('.')[0]
+        # store feature
+        features[image_id] = feature
+        print('>%s' % name)
+    return features
+
+
+urllib.request.urlretrieve(sys.argv[1], "./demo/ex.jpg")
+# extract features from all images
+directory = 'demo'
+features = extract_features(directory)
+print('Extracted Features: %d' % len(features))
+print(features)
+# save to file
+dump(features, open('features_demo.pkl', 'wb'))
+
+
+# load doc into memory
+
+
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+# load a pre-defined list of photo identifiers
+
+
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+    # process line by line
+    for line in doc.split('\n'):
+        # skip empty lines
+        if len(line) < 1:
+            continue
+        # get the image identifier
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+# load clean descriptions into memory
+
+
+def load_clean_descriptions(filename, dataset, verbose):
+    # load document
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        if verbose == 1:
+            print(line)
+
+        if len(line) == 0:
+            continue
+        # split line by white space
+        tokens = line.split()
+        if verbose == 1:
+            print(tokens)
+        # split id from description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # skip images not in the set
+        if image_id in dataset:
+            # create list
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+            # wrap description in tokens
+            desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+            # store
+            descriptions[image_id].append(desc)
+    return descriptions
+
+# load photo features
+
+
+def load_photo_features(filename, dataset):
+    # load all features
+    all_features = load(open(filename, 'rb'))
+    # filter features
+    features = {k: all_features[k] for k in dataset}
+    return features
+
+# covert a dictionary of clean descriptions to a list of descriptions
+
+
+def to_lines(descriptions):
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+
+# fit a tokenizer given caption descriptions
+
+
+def create_tokenizer(descriptions):
+    lines = to_lines(descriptions)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+# calculate the length of the description with the most words
+
+
+def max_length(descriptions):
+    lines = to_lines(descriptions)
+    return max(len(d.split()) for d in lines)
+
+# map an integer to a word
+
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+# generate a description for an image
+
+
+def generate_desc(model, tokenizer, photo, max_length):
+    # seed the generation process
+    in_text = 'startseq'
+    # iterate over the whole length of the sequence
+    for i in range(max_length):
+        # integer encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad input
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        # predict next word
+        yhat = model.predict([photo, sequence], verbose=0)
+        # convert probability to integer
+        yhat = argmax(yhat)
+        # map integer to word
+        word = word_for_id(yhat, tokenizer)
+        # stop if we cannot map the word
+        if word is None:
+            break
+        # append as input for generating the next word
+        in_text += ' ' + word
+        # stop if we predict the end of the sequence
+        if word == 'endseq':
+            break
+    return in_text
+
+# evaluate the skill of the model
+
+
+def evaluate_model(model, descriptions, photos, tokenizer, max_length):
+    actual, predicted = list(), list()
+    # step over the whole set
+    for key, desc_list in descriptions.items():
+        # generate description
+        yhat = generate_desc(model, tokenizer, photos[key], max_length)
+        # store actual and predicted
+        references = [d.split() for d in desc_list]
+        actual.append(references)
+        predicted.append(yhat.split())
+    # calculate BLEU score
+    print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
+    print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
+    print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
+    print('BLEU-4: %f' %
+          corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
+
+
+def show_us(model, descriptions, photos, tokenizer, max_length):
+    # step over the whole set
+    for key, desc_list in descriptions.items():
+        print(key)
+        # generate description
+        yhat = generate_desc(model, tokenizer, photos[key], max_length)
+        # store actual and predicted
+        print(yhat)
+
+# prepare tokenizer on train set
+
+
+# load training dataset (6K)
+filename = 'Flickr8k_text/Flickr_8k.trainImages.txt'
+train = load_set(filename)
+print('Dataset: %d' % len(train))
+# descriptions
+train_descriptions = load_clean_descriptions('descriptions.txt', train, 0)
+print('Descriptions: train=%d' % len(train_descriptions))
+# prepare tokenizer
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+# determine the maximum sequence length
+max_length = max_length(train_descriptions)
+print('Description Length: %d' % max_length)
+
+# prepare test set
+
+# load test set
+filename = 'demolist.txt'
+test = load_set(filename)
+print('Dataset: %d' % len(test))
+# descriptions
+test_descriptions = load_clean_descriptions('descriptions_demo.txt', test, 0)
+print('Descriptions: test=%d' % len(test_descriptions))
+# photo features
+test_features = load_photo_features('features_demo.pkl', test)
+print('Photos: test=%d' % len(test_features))
+
+# load the model
+filename = 'model-ep005-loss3.540-val_loss3.840.h5'
+model = load_model(filename)
+# evaluate model
+# evaluate_model(model, test_descriptions, test_features, tokenizer, max_length)
+show_us(model, test_descriptions, test_features, tokenizer, max_length)
+
